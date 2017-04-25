f6c5305a132828096b6cbcd19d85b7327a0a2f1d
Sonja <sonjazivkovic@hotmail.rs>
2017-04-25 08:57:17 +0200

    Initial navigation driver

--- app/src/main/java/com/example/buca/saxmusicplayer/MainActivity.java
+++ app/src/main/java/com/example/buca/saxmusicplayer/MainActivity.java
@@ -1,20 +1,51 @@
 package com.example.buca.saxmusicplayer;
 
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.buca.saxmusicplayer.activities.ChoosePlaylistActivity;
 import com.example.buca.saxmusicplayer.activities.LoadAllSongsActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String[] menuItems;
+    private ListView drawerList;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle drawerToggle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        menuItems = new String[] { getString(R.string.all_songs), getString(R.string.default_playlist), getString(R.string.office_playlist_placeholder) };
+        drawerList = (ListView) findViewById(R.id.left_drawer);
+        drawerList.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, menuItems));
+
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.drawer_open, R.string.drawer_close){
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                Toast.makeText(MainActivity.this, getString(R.string.drawer_closed), Toast.LENGTH_SHORT).show();
+            }
+            public void onDrawerOpened(View view) {
+                super.onDrawerClosed(view);
+                Toast.makeText(MainActivity.this, getString(R.string.drawer_opened), Toast.LENGTH_SHORT).show();
+            }
+        };
+        drawerLayout.setDrawerListener(drawerToggle);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
 
         Button loadAllButton = (Button) findViewById(R.id.loadAllButton);
         loadAllButton.setOnClickListener(new View.OnClickListener() {
@@ -33,7 +64,25 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
 
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
+    }
 
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
