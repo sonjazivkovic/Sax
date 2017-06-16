59cbcac4f91c25a3b6cca054abe58dc6fdff57c1
stojan-mitric-devtech <stojan.mitric.contractor@ensighten.com>
2017-06-16 14:59:29 +0200

    Added Rate app to about activity

--- app/src/main/java/com/example/buca/saxmusicplayer/activities/AboutActivity.java
+++ app/src/main/java/com/example/buca/saxmusicplayer/activities/AboutActivity.java
@@ -3,6 +3,13 @@ package com.example.buca.saxmusicplayer.activities;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.RatingBar.OnRatingBarChangeListener;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.buca.saxmusicplayer.R;
 
@@ -11,6 +18,11 @@ import com.example.buca.saxmusicplayer.R;
  */
 
 public class AboutActivity  extends AppCompatActivity {
+
+    private RatingBar ratingBar;
+    private TextView txtRatingValue;
+    private Button btnSubmit;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,5 +34,47 @@ public class AboutActivity  extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         setTitle(R.string.about_app);
+
+        addListenerOnRatingBar();
+        addListenerOnButton();
+
+
+    }
+
+    public void addListenerOnRatingBar() {
+
+        ratingBar = (RatingBar) findViewById(R.id.ratingBar);
+        txtRatingValue = (TextView) findViewById(R.id.txtRatingValue);
+
+        //ako se promeni broj zvezdica, prikazuje automatski u viewu
+        ratingBar.setOnRatingBarChangeListener(new OnRatingBarChangeListener() {
+            public void onRatingChanged(RatingBar ratingBar, float rating,
+                                        boolean fromUser) {
+
+                txtRatingValue.setText(String.valueOf(rating));
+
+            }
+        });
+    }
+
+    public void addListenerOnButton() {
+
+        ratingBar = (RatingBar) findViewById(R.id.ratingBar);
+        btnSubmit = (Button) findViewById(R.id.btnSubmit);
+
+        //kad se klikne na dugme, pokaze se rejting.
+        btnSubmit.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(AboutActivity.this,
+                        String.valueOf(ratingBar.getRating()),
+                        Toast.LENGTH_SHORT).show();
+
+            }
+
+        });
+
     }
 }
